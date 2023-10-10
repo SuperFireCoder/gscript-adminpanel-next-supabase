b2aef480f9bdebc5313982f482049c14f6ff2343
Daman Huri <damanhuri0119@gmail.com>
2023-10-11 00:20:03 +0200

    Create layout component

---
+++ src/components/Layout/layout.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import Loader from "@/components/common/Loader";
+
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex h-screen overflow-hidden">
+          {/* <!-- ===== Sidebar Start ===== --> */}
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Sidebar End ===== --> */}
+
+          {/* <!-- ===== Content Area Start ===== --> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* <!-- ===== Header Start ===== --> */}
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* <!-- ===== Header End ===== --> */}
+
+            {/* <!-- ===== Main Content Start ===== --> */}
+            <main>
+              <div className="mx-auto max-w-screen-2xl px-9 py-11.5">
+                {children}
+              </div>
+            </main>
+            {/* <!-- ===== Main Content End ===== --> */}
+          </div>
+          {/* <!-- ===== Content Area End ===== --> */}
+        </div>
+      )}
+      {/* <!-- ===== Main Footer Start ===== --> */}
+      <Footer />
+      {/* <!-- ===== Main Footer End ===== --> */}
+    </div>
+  );
+}
