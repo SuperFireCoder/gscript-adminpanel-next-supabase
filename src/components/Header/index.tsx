bdd708036c8025128b27122e0646ecfd9d74c8f7
SuperDev0119 <keith.corrigan217@outlook.com>
2023-10-11 22:14:32 +0200

    Add new primary color to tailwind config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = (props: {
       <div className="flex flex-grow items-center justify-end px-9 py-4 shadow-2">
         <div className="flex flex-col gap-1">
           <div className="flex gap-3 items-center">
-            <span className="bg-primary text-white text-xs py-0.5 px-2 leading-5 rounded-full">
+            <span className="bg-primary2 text-white text-xs py-0.5 px-2 leading-5 rounded-full">
               Super Admin
             </span>
             <span className="text-sm">ben@writersroomberlin.de</span>
