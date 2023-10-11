1ff88e8684c692423e76fd8055ccc068881717b1
Daman Huri <damanhuri0119@gmail.com>
2023-10-11 22:14:54 +0200

    Create Admin dashboard with dynamic routing and create page

---
+++ src/pages/admin/[slug].tsx
@@ -0,0 +1,75 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+import RootLayout from "@/components/Layout/layout";
+import Modal from "@/components/Modal";
+
+const User = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return (
+    <RootLayout>
+      <div className="col-span-12">
+        <div className="rounded-sm border border-gray bg-white shadow-default">
+          <div className="flex justify-between border-b border-gray px-7 py-4.5">
+            <h4 className="text-base font-medium text-primary">
+              Edit Admin User
+            </h4>
+          </div>
+          <div className="flex flex-col gap-7.5 p-7.5 pb-11 text-secondary">
+            <div className="flex gap-7.5">
+              <div className="flex-1">
+                <div className="text-sm font-medium mb-3">Email Address*</div>
+                <div className="flex items-center text-base font-medium h-11.5">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
+                  />
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="text-sm font-medium mb-3">Password*</div>
+                <div className="flex items-center text-base font-medium h-11.5">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-between">
+              <button className="inline-flex items-center justify-center gap-2.5 rounded-full border border-primary2 py-2 px-6 text-center font-medium text-primary2 hover:bg-opacity-90">
+                <Image
+                  width={18}
+                  height={18}
+                  src={"/images/icon-trash.svg"}
+                  alt="Delete User"
+                />
+                Delete User
+              </button>
+              <div className="flex gap-7.5">
+                <button
+                  className="rounded-full border border-primary2 py-2 px-6 text-center font-medium text-primary2 hover:bg-opacity-90"
+                  onClick={() => router.push("/admin")}
+                >
+                  Cancel
+                </button>
+                <button className="rounded-full border border-primary2 bg-primary2 py-2 px-6 text-center font-medium text-white hover:bg-opacity-90">
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RootLayout>
+  );
+};
+
+export default User;
---
+++ src/pages/admin/create/index.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from "next/router";
+import RootLayout from "@/components/Layout/layout";
+
+const AdminCreate = () => {
+  const router = useRouter();
+  return (
+    <RootLayout>
+      <div className="col-span-12">
+        <div className="rounded-sm border border-gray bg-white shadow-default">
+          <div className="flex justify-between border-b border-gray px-7 py-4.5">
+            <h4 className="text-base font-medium text-primary">
+              Add a New Admin User
+            </h4>
+          </div>
+          <div className="flex flex-col gap-7.5 p-7.5 pb-11 text-secondary">
+            <div className="flex gap-7.5">
+              <div className="flex-1">
+                <div className="text-sm font-medium mb-3">Email Address*</div>
+                <div className="flex items-center text-base font-medium h-11.5">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
+                  />
+                </div>
+              </div>
+              <div className="flex-1">
+                <div className="text-sm font-medium mb-3">Password*</div>
+                <div className="flex items-center text-base font-medium h-11.5">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <div className="flex gap-7.5">
+                <button
+                  className="rounded-full border border-primary2 py-2 px-6 text-center font-medium text-primary2 hover:bg-opacity-90"
+                  onClick={() => router.push("/admin")}
+                >
+                  Cancel
+                </button>
+                <button className="rounded-full border border-primary2 bg-primary2 py-2 px-6 text-center font-medium text-white hover:bg-opacity-90">
+                  Add Admin
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RootLayout>
+  );
+};
+
+export default AdminCreate;
