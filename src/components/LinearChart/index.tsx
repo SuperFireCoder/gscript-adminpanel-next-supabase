84fc0fc2139791ae94bd90d39f457749566077a7
Daman Huri <damanhuri0119@gmail.com>
2023-10-12 19:07:17 +0200

    Create chart components

---
+++ src/components/LinearChart/index.tsx
@@ -0,0 +1,142 @@
+"use client";
+import { ApexOptions } from "apexcharts";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+interface LinearChart {
+  series: { data: number[] }[];
+}
+
+const LinearChart: React.FC = () => {
+  const [state, setState] = useState<LinearChart>({
+    series: [
+      {
+        data: [
+          168, 385, 201, 298, 187, 195, 291, 110, 215, 390, 280, 112, 123, 212,
+          270, 190, 310, 115, 90, 380, 112, 223, 292, 170, 290, 110, 115, 290,
+          380, 312,
+        ],
+      },
+    ],
+  });
+
+  // Update the state
+  const updateState = () => {
+    setState((prevState) => ({
+      ...prevState,
+      // Update the desired properties
+    }));
+  };
+  updateState;
+
+  const options: ApexOptions = {
+    colors: ["#052052"],
+    chart: {
+      type: "bar",
+      height: 350,
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "55%",
+        // endingShape: "rounded",
+        borderRadius: 2,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 4,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23",
+        "24",
+        "25",
+        "26",
+        "27",
+        "28",
+        "29",
+        "30",
+      ],
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    legend: {
+      show: true,
+      position: "top",
+      horizontalAlign: "left",
+      fontFamily: "inter",
+
+      markers: {
+        radius: 99,
+      },
+    },
+    // yaxis: {
+    //   title: false,
+    // },
+    grid: {
+      yaxis: {
+        lines: {},
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+
+    tooltip: {
+      x: {
+        show: false,
+      },
+      // y: {
+      //   formatter: function (val) {
+      //     return val;
+      //   },
+      // },
+    },
+  };
+
+  return (
+    <ApexCharts
+      options={options}
+      series={state.series}
+      type="bar"
+      height={300}
+    />
+  );
+};
+
+export default LinearChart;
