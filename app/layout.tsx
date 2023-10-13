6fdea9cd8026cb43305825f8ed160e4687cb63c3
Daman Huri <damanhuri0119@gmail.com>
2023-10-13 21:47:30 +0200

    Implement supabase auth in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,56 @@
-"use client";
 import "./globals.css";
 import "./data-tables-css.css";
 import "./fonts.css";
 
-import { useState } from "react";
-import { usePathname } from "next/navigation";
+import { headers } from "next/headers";
 
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function RootLayout({
+import AuthProvider from "../components/AuthProvider";
+
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const pathname = usePathname();
+  const headersList = headers();
+  const pathname = headersList.get("x-url") || "";
+
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const accessToken = session?.access_token || null;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user && !pathname.includes("login")) {
+    redirect("/login");
+  }
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        {pathname === "/login" ? (
+        {pathname.includes("/login") ? (
           <>{children}</>
         ) : (
           <div>
             <div className="flex overflow-hidden">
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
+              <Sidebar />
               <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                <Header
-                  sidebarOpen={sidebarOpen}
-                  setSidebarOpen={setSidebarOpen}
-                />
+                <Header />
                 <main>
                   <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                    {children}
+                    <AuthProvider accessToken={accessToken}>
+                      {children}
+                    </AuthProvider>
                   </div>
                 </main>
               </div>
