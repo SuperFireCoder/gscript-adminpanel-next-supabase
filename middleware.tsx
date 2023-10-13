f731f940794ce71e65a19924fa536fbd04dce48d
SuperDev0119 <keith.corrigan217@outlook.com>
2023-10-13 21:45:49 +0200

    Create supabase middleware

---
+++ middleware.tsx
@@ -0,0 +1,20 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { Database } from "./types/database.types";
+
+// this middleware refreshes the user's session and must be run
+// for any Server Component route that uses `createServerComponentSupabaseClient`
+export async function middleware(req: NextRequest) {
+  const requestHeaders = new Headers(req.headers);
+  requestHeaders.set("x-url", req.url);
+
+  const res = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  const supabase = createMiddlewareClient<Database>({ req, res });
+  await supabase.auth.getSession();
+  return res;
+}
