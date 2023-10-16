74186d38c5b9364f9d4041637c5a43512b1d00e0
Daman Huri <damanhuri0119@gmail.com>
2023-10-16 21:49:44 +0200

    Update database.types.ts content

--- types/database.types.ts
+++ types/database.types.ts
@@ -4,56 +4,56 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       subscription: {
         Row: {
-          created_at: string
-          end: string | null
-          id: number
-          name: string | null
-          role: string | null
-          start: string | null
-          type: string | null
-          user_id: string | null
-        }
+          created_at: string;
+          end: string | null;
+          id: number;
+          name: string | null;
+          role: string | null;
+          start: string | null;
+          type: string | null;
+          user_id: string | null;
+        };
         Insert: {
-          created_at?: string
-          end?: string | null
-          id?: number
-          name?: string | null
-          role?: string | null
-          start?: string | null
-          type?: string | null
-          user_id?: string | null
-        }
+          created_at?: string;
+          end?: string | null;
+          id?: number;
+          name?: string | null;
+          role?: string | null;
+          start?: string | null;
+          type?: string | null;
+          user_id?: string | null;
+        };
         Update: {
-          created_at?: string
-          end?: string | null
-          id?: number
-          name?: string | null
-          role?: string | null
-          start?: string | null
-          type?: string | null
-          user_id?: string | null
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string;
+          end?: string | null;
+          id?: number;
+          name?: string | null;
+          role?: string | null;
+          start?: string | null;
+          type?: string | null;
+          user_id?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
