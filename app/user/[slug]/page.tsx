002d94228c210467646272a7438ebbaed7a2c77d
SuperDev0119 <keith.corrigan217@outlook.com>
2023-10-16 21:50:57 +0200

    Implement supabase into user panel

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -4,18 +4,39 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-const UserEditPage = async () => {
-  const supabase = createServerComponentClient({ cookies });
+import { supabase as client_supabase } from "../../../lib/supabase";
+import { User } from "../../../types/user";
 
+const UserEditPage = async ({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) => {
+  // Auth user
+  const supabase = createServerComponentClient({ cookies });
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
   if (!user) {
     redirect("/login");
   }
 
-  return <UserEditForm />;
+  // Get user data by slug
+  const { data, error } = await client_supabase
+    .from("users")
+    .select("id, email, created_at, subscription(name, role, type, start, end)")
+    .eq("id", slug);
+  const user_data =
+    data && !error
+      ? data.map((user: any) => ({
+          id: user.id,
+          email: user.email,
+          created_at: user.created_at,
+          ...user.subscription[0],
+        }))
+      : [];
+
+  return <UserEditForm user={user_data} />;
 };
 
 export default UserEditPage;
--- components/UserEdit/index.tsx
+++ components/UserEdit/index.tsx
@@ -5,7 +5,13 @@ import Image from "next/image";
 import Modal from "../Modal/UserModal";
 import Link from "next/link";
 
-const UserEditForm = () => {
+import { User } from "../../types/user";
+
+interface Props {
+  user: User[];
+}
+
+const UserEditForm = ({ user }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
@@ -17,68 +23,78 @@ const UserEditForm = () => {
           </div>
           <div className="flex flex-col gap-7.5 p-7.5 pb-11 text-secondary">
             <div className="flex gap-7.5">
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">Name</div>
-                <div className="flex items-center text-base font-medium h-11.5">
-                  John Doe
+              <div className="flex flex-col gap-7.5 w-1/2">
+                <div>
+                  <div className="text-sm font-medium mb-3">Name</div>
+                  <div className="flex items-center text-base font-medium h-11.5">
+                    {user[0].name}
+                  </div>
                 </div>
-              </div>
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">
-                  Current Subscription
+
+                <div>
+                  <div className="text-sm font-medium mb-3">Email Address</div>
+                  <div className="flex items-center text-base font-medium h-11.5">
+                    {user[0].email}
+                  </div>
                 </div>
-                <div className="flex items-center gap-5 text-base font-medium h-11.5">
-                  Standard
-                  <button className="inline-flex items-center justify-center gap-2.5 rounded-full border border-primary2 pt-2 pb-2.5 px-6 text-center font-medium text-primary2 hover:bg-opacity-90">
-                    <Image
-                      width={18}
-                      height={18}
-                      src={"/images/cancel-subscription.svg"}
-                      alt="Cancel Subscription"
+
+                <div>
+                  <div className="text-sm font-medium mb-3">New Password</div>
+                  <div className="flex items-center text-base font-medium h-11.5">
+                    <input
+                      type="text"
+                      name="price"
+                      id="price"
+                      className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
                     />
-                    Cancel Subscription
-                  </button>
+                  </div>
                 </div>
+              </div>
+
+              <div className="flex flex-col gap-7.5 w-1/2">
+                <div>
+                  <div className="text-sm font-medium mb-3">
+                    Current Subscription
+                  </div>
+                  <div className="flex items-center gap-5 text-base font-medium h-11.5">
+                    {user[0].type}
+                    {user[0].type !== "None" && (
+                      <button className="inline-flex items-center justify-center gap-2.5 rounded-full border border-primary2 pt-2 pb-2.5 px-6 text-center font-medium text-primary2 hover:bg-opacity-90">
+                        <Image
+                          width={18}
+                          height={18}
+                          src={"/images/cancel-subscription.svg"}
+                          alt="Cancel Subscription"
+                        />
+                        Cancel Subscription
+                      </button>
+                    )}
+                  </div>
+                </div>
+
+                {user[0].type !== "None" && (
+                  <>
+                    <div>
+                      <div className="text-sm font-medium mb-3">
+                        SubScription Start
+                      </div>
+                      <div className="flex items-center text-sm font-medium h-11.5">
+                        {user[0].start.toString()}
+                      </div>
+                    </div>
+
+                    <div>
+                      <div className="text-sm font-medium mb-3">
+                        SubScription End
+                      </div>
+                      <div className="flex items-center text-sm font-medium h-11.5">
+                        {user[0].end.toString()}
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
-
-            <div className="flex gap-7.5">
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">Email Address</div>
-                <div className="flex items-center text-base font-medium h-11.5">
-                  john.doe@gmail.com
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">
-                  SubScription Start
-                </div>
-                <div className="flex items-center text-sm font-medium h-11.5">
-                  08/08/2023
-                </div>
-              </div>
-            </div>
-
-            <div className="flex gap-7.5">
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">New Password</div>
-                <div className="flex items-center text-base font-medium h-11.5">
-                  <input
-                    type="text"
-                    name="price"
-                    id="price"
-                    className="block w-full h-full rounded-md border-0 px-3 py-1.5 text-primary ring-1 ring-gray ring-inset focus:ring-2 focus:ring-inset"
-                  />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="text-sm font-medium mb-3">SubScription End</div>
-                <div className="flex items-center text-sm font-medium h-11.5">
-                  07/09/2023
-                </div>
-              </div>
-            </div>
-
             <div className="flex justify-between">
               <button
                 className="inline-flex items-center justify-center gap-2.5 rounded-full border border-primary2 py-2 px-6 text-center font-medium text-primary2 hover:bg-opacity-90"
