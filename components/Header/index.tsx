9a502521d6fe7a5938ef34d123c285292b297bee
SuperDev0119 <keith.corrigan217@outlook.com>
2023-10-16 10:48:49 +0200

    Update header with custom data

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,70 +1,68 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const Header = (props: {
-  sidebarOpen: string | boolean | undefined;
-  setSidebarOpen: (arg0: boolean) => void;
-}) => {
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  user: any;
+}
+
+const Header = ({ user }: Props) => {
+  const supabase = createClientComponentClient();
+  const pathname = usePathname();
+
+  const handleLogOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("ERROR:", error);
+    }
+  };
+
   return (
-    <header className="sticky h-20 top-0 z-10 flex w-full bg-white drop-shadow-1">
-      <div className="flex flex-grow items-center justify-between px-9 py-4 shadow-2">
-        <div>
-          <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-            {/* <!-- Hamburger Toggle BTN --> */}
-            <button
-              aria-controls="sidebar"
-              onClick={(e) => {
-                e.stopPropagation();
-                props.setSidebarOpen(!props.sidebarOpen);
-              }}
-              className="block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+    <>
+      {pathname.includes("/login") ? (
+        <>
+          <header className="flex justify-between items-center h-20 bg-primary px-10 xl:px-18 2xl:px-34">
+            <Image
+              width={72}
+              height={59}
+              src={"/images/logo/logo.svg"}
+              alt="Logo"
+            />
+            <Link
+              href="#"
+              className="inline-flex items-center justify-center rounded-full bg-secondary py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
             >
-              <span className="relative block h-5.5 w-5.5 cursor-pointer">
-                <span className="du-block absolute right-0 h-full w-full">
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                      !props.sidebarOpen && "!w-full delay-300"
-                    }`}
-                  ></span>
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                      !props.sidebarOpen && "delay-400 !w-full"
-                    }`}
-                  ></span>
-                  <span
-                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                      !props.sidebarOpen && "!w-full delay-500"
-                    }`}
-                  ></span>
-                </span>
-                <span className="absolute right-0 h-full w-full rotate-45">
-                  <span
-                    className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                      !props.sidebarOpen && "!h-0 !delay-[0]"
-                    }`}
-                  ></span>
-                  <span
-                    className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                      !props.sidebarOpen && "!h-0 !delay-200"
-                    }`}
-                  ></span>
-                </span>
-              </span>
-            </button>
-            {/* <!-- Hamburger Toggle BTN --> */}
-          </div>
-        </div>
-        <div className="flex flex-col gap-1">
-          <div className="flex gap-3 items-center">
-            <span className="bg-primary2 text-white text-xs py-0.5 px-2 leading-5 rounded-full">
-              Super Admin
-            </span>
-            <span className="text-sm">ben@writersroomberlin.de</span>
-          </div>
-          <div className="text-right text-gray text-xs">Logout</div>
-        </div>
-      </div>
-    </header>
+              Visit the Main Site
+            </Link>
+          </header>
+        </>
+      ) : (
+        <>
+          <header className="sticky h-20 top-0 z-10 flex w-full bg-white drop-shadow-1">
+            <div className="flex flex-grow items-center justify-between px-9 py-4 shadow-2">
+              <div></div>
+              <div className="flex flex-col gap-1">
+                <div className="flex gap-3 items-center">
+                  <span className="bg-primary2 text-white text-xs py-0.5 px-2 leading-5 rounded-full">
+                    Super Admin
+                  </span>
+                  <span className="text-sm">{user?.email}</span>
+                </div>
+                <div
+                  className="text-right text-gray text-xs cursor-pointer"
+                  onClick={handleLogOut}
+                >
+                  Logout
+                </div>
+              </div>
+            </div>
+          </header>
+        </>
+      )}
+    </>
   );
 };
 
