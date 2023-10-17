42f291cb1e755fcf7a3c11e1a84c04599cfe9705
Daman Huri <damanhuri0119@gmail.com>
2023-10-17 17:46:41 +0200

    Update sidebar fir show and hide

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -19,45 +19,11 @@ const Sidebar = () => {
     setSidebarOpen(!isLogin);
   }, [pathname]);
 
-  // const trigger = useRef<any>(null);
-  // const sidebar = useRef<any>(null);
-
-  // let storedSidebarExpanded = "true";
-  // const [sidebarExpanded, setSidebarExpanded] = useState(
-  //   storedSidebarExpanded === null ? false : storedSidebarExpanded === "true"
-  // );
-
-  // close on click outside
-  // useEffect(() => {
-  //   const clickHandler = ({ target }: MouseEvent) => {
-  //     if (!sidebar.current || !trigger.current) return;
-  //     if (
-  //       !sidebarOpen ||
-  //       sidebar.current.contains(target) ||
-  //       trigger.current.contains(target)
-  //     )
-  //       return;
-  //     setSidebarOpen(false);
-  //   };
-  //   document.addEventListener("click", clickHandler);
-  //   return () => document.removeEventListener("click", clickHandler);
-  // });
-
-  // useEffect(() => {
-  //   localStorage.setItem("sidebar-expanded", sidebarExpanded.toString());
-  //   if (sidebarExpanded) {
-  //     document.querySelector("body")?.classList.add("sidebar-expanded");
-  //   } else {
-  //     document.querySelector("body")?.classList.remove("sidebar-expanded");
-  //   }
-  // }, [sidebarExpanded]);
-
   return (
     <aside
-      // className={`absolute left-0 top-0 z-20 flex w-70 flex-col overflow-y-hidden bg-primary duration-300 ease-linear -translate-x-full ${
-      //   sidebarOpen && "lg:static lg:translate-x-0"
-      // }`}
-      className={`z-20 fixed top-0 h-full flex flex-col overflow-x-hidden bg-primary duration-300 ease-linear w-70 -left-70 lg:left-0`}
+      className={`absolute left-0 top-0 z-20 flex h-full w-70 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark -translate-x-full ${
+        sidebarOpen ? "lg:static lg:translate-x-0" : ""
+      }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-center gap-2 px-6.5 py-6 lg:py-6.5">
